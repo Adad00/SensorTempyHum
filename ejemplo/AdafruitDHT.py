f14602790fabc95d860aa6d87e5fb970a178df00
Adad00 <adadjiramosoriohernandez9@gmail.com>
2016-05-17 14:07:07 -0500

    Simplify the code

--- ejemplo/AdafruitDHT.py
+++ ejemplo/AdafruitDHT.py
@@ -1,24 +1,11 @@
 import sys
-
 import Adafruit_DHT
 
+sensor = Adafruit_DHT.AM2302
+pin = 4
 
-sensor_args = { '11': Adafruit_DHT.DHT11,
-                '22': Adafruit_DHT.DHT22,
-                '2302': Adafruit_DHT.AM2302 }
-if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
-    sensor = sensor_args[sys.argv[1]]
-    pin = sys.argv[2]
-else:
-    print('uso: sudo ./Adafruit_DHT.py [11|22|2302] GPIOpin#')
-    print('ejemplo: sudo ./Adafruit_DHT.py 2302 4 - Lee de un sensor AM2302 conectado al GPIO #4')
-    sys.exit(1)
+humedad, temperatura = Adafruit_DHT.read_retry(sensor, pin)
 
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+print('Temperatura={0:0.1f}*C  Humedad={1:0.1f}%'.format(temperatura, humedad))
 
-
-if humidity is not None and temperature is not None:
-    print('Temperatura={0:0.1f}*C  Humedad={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Fallo al leer los datos. Intentalo nuevamente!')
-    sys.exit(1)
+sys.exit(1)
